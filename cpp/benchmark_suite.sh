147dd25961aa4bc96c13ab25a30a0709133010be
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-05-31 00:03:54 -0400

    Adjust benchmark suite params

--- cpp/benchmark_suite.sh
+++ cpp/benchmark_suite.sh
@@ -2,14 +2,14 @@
 
 make
 
-for NUM_LOOKUP in 1 16 64; do
-  for POOL_SIZE in 1 16 64; do
+for NUM_LOOKUP in 1 4 16; do
+  for POOL_SIZE in 16 32 64; do
 
     ./matchbench \
       -NUM_LOOKUP_IN_TASK_MIX "${NUM_LOOKUP}" \
       -NUM_REGULATION_IN_TASK_MIX 1 \
-      -TARGET_POOL_SIZE "$((32 * POOL_SIZE))" \
-      -QUERY_POOL_SIZE "$((16 * POOL_SIZE))"
-
+      -TARGET_POOL_SIZE "$((4 * POOL_SIZE))" \
+      -QUERY_POOL_SIZE "$((1 * POOL_SIZE))" \
+      -NUM_DEPOSITORIES 16
   done
 done
